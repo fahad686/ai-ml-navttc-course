7e47ab1dc3c766cc13e5c6e94829f0d10d8ec2d2
Fahad Ali <fahadofficial686@gmail.com>
2025-09-03 20:30:44 +0500

    variables, math library

--- my_first_project/main.py
+++ my_first_project/main.py
@@ -1,1 +1,20 @@
 print("Hello Python Developer")
+
+
+#Variable
+a=5
+b=3
+
+#artihmatic
+print('Addition: ',a+b)
+print('substraction: ',a-b)
+print('multiplicaiton: ',a*b)
+print('division: ',a/b)
+print('Reminder: ',a%b)
+print('Exponent: ',a**2)
+
+#if we want to use math thing so we want to math library
+
+import math
+print('cos: ',math.cos(a))
+print('Square of a: ',math.sqrt(a))
