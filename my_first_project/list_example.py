842a090cf414dac7c4eeb45ee374dd7cb810b2ce
Fahad Ali <fahadofficial686@gmail.com>
2025-09-05 19:51:44 +0500

    list some func

--- my_first_project/list_example.py
+++ my_first_project/list_example.py
@@ -2,9 +2,42 @@
 fruits=['apple','banana','mango','cherry','stawberry']
 print(fruits)
 
+print(fruits[-3: -1])
 
-ages=['young',33,35,78,48]
+
+ages=['young','cherry',35,78,48]
 print(ages)
 
-print(ages[0])
+#accessing string by index and their charc
+print(ages[0][1])
 print(ages[-2])
+
+print(ages[1][2])
+
+
+#upate list
+fruits=['mango','appel']
+print(fruits)
+
+#replacing
+fruits[1]='banana'
+print(fruits)
+
+
+#adding
+fruits.insert(2,'stawberry')
+print(fruits)
+fruits.insert(2,'grapps')
+print(fruits)
+
+
+#append method to use adding element on end
+fruits.append('samosa')
+print(fruits)
+
+#change akhty with slicing
+fruits[1:3:]=['amrood','malta']
+print(fruits)
+
+fruits1=fruits
+print("assigning list",fruits1)
