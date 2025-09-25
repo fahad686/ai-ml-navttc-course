ab8cfa4ad936665c94448f22fb627ea2de33010b
Fahad Ali <fahadofficial686@gmail.com>
2025-09-25 20:36:09 +0500

    decorator, asyn programming,exeption_handling

--- classes_work/week_4/decorator_example.py
+++ classes_work/week_4/decorator_example.py
@@ -1,11 +1,19 @@
-#In Python, a decorator is a powerful design pattern that allows you to add functionality to an existing function or class without modifying its source code. This is achieved by "wrapping" the original function with another function that takes the original function as an argument and returns a new, modified version.
-# This is made possible because Python treats functions as first-class objects, meaning they can be passed as arguments, assigned to variables, and returned from other functions, just like any other data type.
+#In Python, a decorator is a powerful design pattern that allows you to add functionality to
+# an existing function or class without modifying its source code. This is achieved by "wrapping"
+# the original function with another function that takes the original function as an argument and returns
+# a new, modified version.
+# This is made possible because Python treats functions as first-class
+# objects, meaning they can be passed as arguments, assigned to variables, and returned from other
+# functions, just like any other data type.
+
+
 # 1. Define the decorator function
 def my_decorator(func):
     def wrapper():
         print("Something is happening before the function is called.")
         func()
         print("Something is happening after the function is called.")
+        func()
     return wrapper
 
 # 2. Define the function to be decorated
@@ -13,5 +21,12 @@ def my_decorator(func):
 def say_whee():
     print("Whee!")
 
+@my_decorator
+def say_quite():
+    print('Keep quite class!')
+
+
 # 3. Call the decorated function
 say_whee()
+
+say_quite()
